46c23defe2310a4731c1c67491d659300423c926
Adrian0 <Adrian0@ADRIANO>
2025-09-06 18:57:40 -0300

    Add project files.

---
+++ MinhaAPI/Domain/Catergoria.cs
@@ -0,0 +1,10 @@
+﻿namespace MinhaAPI.Domain
+{
+    public class Catergoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemURL { get; set; }
+
+    }
+}

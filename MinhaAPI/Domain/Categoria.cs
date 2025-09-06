0757fe64b74c2ae267d94df577c60ae77286e659
Adriano-Guedes <adrianno.aguedes@g,ail.com>
2025-09-06 20:10:42 -0300

    Aplicando data notattions e rodando primeira migration

--- MinhaAPI/Domain/Categoria.cs
+++ MinhaAPI/Domain/Categoria.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MinhaAPI.Domain
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
@@ -9,8 +12,13 @@ namespace MinhaAPI.Domain
             Produtos = new Collection<Produto>();
         }
 
+        [Key]
         public int CategoriaId { get; set; }
+        [Required]
+        [StringLength(255)]
         public string? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
         public string? ImagemURL { get; set; }
 
         public ICollection<Produto>? Produtos { get; set; }
